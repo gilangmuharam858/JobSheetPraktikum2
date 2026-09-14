69f6cd35da6cb08dff66a2fc1dfc496aa94801b3
gilangmuharam858 <gilangmuharam858@gmail.com>
2026-09-14 13:37:05 +0700

    Bagian 15

--- script.js
+++ script.js
@@ -590,3 +590,31 @@ console.log(requestQueue.items);
 
 console.log("Request berikutnya:", requestQueue.peek());
 
+// 15.1
+const categoryTree = [
+    {
+        name: "Electronics",
+        children: [
+            {
+                name: "Laptop",
+                children: []
+            },
+            {
+                name: "Phone",
+                children: []
+            }
+        ]
+    }
+];
+
+function printCategories(categories, depth = 0) {
+    for (const category of categories) {
+        console.log(" ".repeat(depth) + category.name);
+
+        if (category.children.length > 0) {
+            printCategories(category.children, depth + 1);
+        }
+    }
+}
+
+printCategories(categoryTree);
