d2dbe2afc9718250c3a8f6424d20c71bb6e3b9b4
gilangmuharam858 <gilangmuharam858@gmail.com>
2026-09-15 21:46:56 +0700

    Bagian 19

--- script.js
+++ script.js
@@ -812,3 +812,26 @@ console.log("Initial State:");
 console.log(state);
 
 render();
+
+// 19.1
+const searchInput = document.querySelector("#search-input");
+
+searchInput.addEventListener("input", (e) => {
+    state.search = e.target.value;
+    render();
+});
+
+const categorySelect = document.querySelector("#category-select");
+
+categorySelect.addEventListener("change", (e) => {
+    state.category = e.target.value;
+    render();
+});
+
+const sortSelect = document.querySelector("#sort-select");
+
+sortSelect.addEventListener("change", (e) => {
+    state.sortBy = e.target.value;
+    render();
+});
+
